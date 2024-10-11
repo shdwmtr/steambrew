be083bd323cd692aae7c725a1d547746753f103e
ShadowMonster <81448108+ShadowMonster99@users.noreply.github.com>
2024-10-11 14:13:29 -0300

    feat: Add GFM to MarkDown Formatter #1
    closes #1

--- src/app/utils/MarkDownConvert.js
+++ src/app/utils/MarkDownConvert.js
@@ -5,10 +5,12 @@ import {unified} from 'unified'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import remarkGfm from 'remark-gfm';
 
 export async function MarkdownToHtml(markdown) {
     const result = await unified()
         .use(remarkParse)
+        .use(remarkGfm)
         .use(remarkRehype, { allowDangerousHtml: false })
         .use(rehypeStringify, { allowDangerousHtml: false })
         .process(markdown)
