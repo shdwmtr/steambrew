368718f93bd6381ab9e2cd5bfaebef80540e97a8
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-01-03 17:30:53 -0400

    fix: Whoops

--- src/app/utils/globals.js
+++ src/app/utils/globals.js
@@ -1,4 +1,4 @@
 
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/steam-brew/us-central1/api' : 'https://api.steambrew.app';
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/steam-brew/us-central1/api' : 'https://steambrew.app';
 
 export { API_URL };
