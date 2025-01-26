01afe492c5e64157ea861a62aeb8b0126094c360
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-01-26 13:39:10 -0400

    chore: Fix indentation

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,12 +5,12 @@ const matter = require('gray-matter');
 
 /** Custom overrides for folder names */
 const CustomNames = {
-  "type-aliases": "Types",
-  "interfaces": "Types",
-  "functions": "Functions",
-  "classes": "Classes",
-  "enumerations": "Enums",
-  "variables": "Variables",
+	"type-aliases": "Types",
+	"interfaces": "Types",
+	"functions": "Functions",
+	"classes": "Classes",
+	"enumerations": "Enums",
+	"variables": "Variables",
 };
 
 function getFilesFromDirectory(dirPath, dirPathBase = '', baseDir = '', topLevelComponents = {}) {
@@ -59,10 +59,10 @@ function getFilesFromDirectory(dirPath, dirPathBase = '', baseDir = '', topLevel
 		
 				if (!topLevelComponents[componentName]) {
 					topLevelComponents[componentName] = {
-					type: 'category',
-					collapsed: true,
-					label: componentName,
-					items: [],
+						type: 'category',
+						collapsed: true,
+						label: componentName,
+						items: [],
 					};
 				}
 				topLevelComponents[componentName].items.push(filePath);
@@ -90,133 +90,126 @@ function getFilesWithComponents(dirPath) {
 const steamBrewClient = getFilesWithComponents('ui/developers/plugins/typescript/client/');
 const steamBrewWebkit = getFilesWithComponents('ui/developers/plugins/typescript/webkit/');
 
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 module.exports = {
     homeSidebar: [
-      "about",
-      {
-        type: 'category',
-        label: 'User Documentation',
-        collapsed: false,
-        link: {
-          type: 'generated-index',
-          description:
-            'A Guide to Getting Acquainted with Millennium.',
-          slug: '/users',
-        },
-        items: [
-          'users/installing',
-          'users/uninstalling',
-          'users/getting-started',
-          'users/trouble-shooting',
-          'users/faq',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Development',
-        collapsed: true,
-        link: {
-          type: 'generated-index',
-          description:
-            "Let's learn about how to contribute to the Millennium community!",
-          slug: '/developers',
-        },
-        items: [
-          {
-            type: 'category',
-            label: 'Creating Themes',
-            collapsed: true,
-            link: {
-              type: 'generated-index',
-              description:
-                "Creating themes for Millennium is a fun and rewarding experience. Let's get started!",
-              slug: '/developers/themes',
-            },
-            items: [
-              'developers/themes/getting-started',
-              'developers/themes/making-themes',
-              'developers/themes/dynamic-patching',
-              'developers/themes/theme-colors',
-              'developers/themes/tips-and-tricks',
-              'developers/themes/publishing',
-            ],
-          },
-          {
-            type: 'category',
-            label: 'Creating Plugins',
-            collapsed: true,
-            link: {
-              type: 'generated-index',
-              description:
-                "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
-              slug: '/developers/plugins',
-            },
-            items: [
-              'developers/plugins/learn',
-              'developers/plugins/packages',
-              {
-                type: 'category',
-                label: 'Python Reference',
-                collapsed: true,
-                link: {
-                  type: 'generated-index',
-                  description:
-                    "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
-                  slug: '/developers/plugins/python',
-                },
-                items: [
-                  'developers/plugins/python/ready',
-                  'developers/plugins/python/steam-path',
-                  'developers/plugins/python/call-method',
-                  'developers/plugins/python/add-css',
-                  'developers/plugins/python/add-js',
-                  'developers/plugins/python/remove-module',
-                  'developers/plugins/python/set-setting',
-                  'developers/plugins/python/user-settings',
-                  'developers/plugins/python/version'
-                ],
-              },
-              {
-                type: 'category',
-                label: 'TypeScript Reference',
-                collapsed: true,
-                link: {
-                  type: 'generated-index',
-                  description:
-                    "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
-                  slug: '/developers/plugins/typescript',
-                },
-                items: [
-                  {
-                    type: 'category',
-                    label: '@steambrew/client',
-                    collapsed: true,
-                    link: {
-                      type: 'generated-index',
-                      description:
-                        "A plugin utility library for the client side of Steam.",
-                      slug: '/developers/plugins/typescript/client',
-                    },
-                    items: steamBrewClient,
-                  },
-				  {
-                    type: 'category',
-                    label: '@steambrew/webkit',
-                    collapsed: true,
-                    link: {
-                      type: 'generated-index',
-                      description:
-                        "A plugin utility library for the web/server side of Steam.",
-                      slug: '/developers/plugins/typescript/webkit',
-                    },
-                    items: steamBrewWebkit,
-                  }
-                ]
-              }
-            ],
-          }
-        ],
-      }
+		"about",
+		{
+			type: 'category',
+			label: 'User Documentation',
+			collapsed: false,
+			link: {
+			type: 'generated-index',
+			description:
+				'A Guide to Getting Acquainted with Millennium.',
+			slug: '/users',
+			},
+			items: [
+				'users/installing',
+				'users/uninstalling',
+				'users/getting-started',
+				'users/trouble-shooting',
+				'users/faq',
+			],
+		},
+		{
+			type: 'category',
+			label: 'Development',
+			collapsed: true,
+			link: {
+				type: 'generated-index',
+				description: "Let's learn about how to contribute to the Millennium community!",
+				slug: '/developers',
+			},
+			items: [
+				{
+					type: 'category',
+					label: 'Creating Themes',
+					collapsed: true,
+					link: {
+					type: 'generated-index',
+					description:
+						"Creating themes for Millennium is a fun and rewarding experience. Let's get started!",
+					slug: '/developers/themes',
+					},
+					items: [
+					'developers/themes/getting-started',
+					'developers/themes/making-themes',
+					'developers/themes/dynamic-patching',
+					'developers/themes/theme-colors',
+					'developers/themes/tips-and-tricks',
+					'developers/themes/publishing',
+					],
+				},
+				{
+					type: 'category',
+					label: 'Creating Plugins',
+					collapsed: true,
+					link: {
+						type: 'generated-index',
+						description: "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
+						slug: '/developers/plugins',
+					},
+					items: [
+						'developers/plugins/learn',
+						'developers/plugins/packages',
+						{
+							type: 'category',
+							label: 'Python Reference',
+							collapsed: true,
+							link: {
+								type: 'generated-index',
+								description: "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
+								slug: '/developers/plugins/python',
+							},
+							items: [
+								'developers/plugins/python/ready',
+								'developers/plugins/python/steam-path',
+								'developers/plugins/python/call-method',
+								'developers/plugins/python/add-css',
+								'developers/plugins/python/add-js',
+								'developers/plugins/python/remove-module',
+								'developers/plugins/python/set-setting',
+								'developers/plugins/python/user-settings',
+								'developers/plugins/python/version'
+							],
+						},
+						{
+							type: 'category',
+							label: 'TypeScript Reference',
+							collapsed: true,
+							link: {
+								type: 'generated-index',
+								description: "Creating plugins for Millennium is a fun and rewarding experience. Let's get started!",
+								slug: '/developers/plugins/typescript',
+							},
+							items: [
+								{
+									type: 'category',
+									label: '@steambrew/client',
+									collapsed: true,
+									link: {
+										type: 'generated-index',
+										description: "A plugin utility library for the client side of Steam.",
+										slug: '/developers/plugins/typescript/client',
+									},
+									items: steamBrewClient,
+								},
+								{
+									type: 'category',
+									label: '@steambrew/webkit',
+									collapsed: true,
+									link: {
+										type: 'generated-index',
+										description: "A plugin utility library for the web/server side of Steam.",
+										slug: '/developers/plugins/typescript/webkit',
+									},
+									items: steamBrewWebkit,
+								}
+							]
+						}
+					],
+				}
+			],
+		}
     ]
 };
