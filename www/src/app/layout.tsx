a4b14ea6011583a8d1fde6a5eb782c605853b03c
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-03-28 13:30:44 -0300

    fix: Fix homepage font

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -34,6 +34,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap" rel="stylesheet" />
+				<link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
+			</head>
 			<body className={'SteamBrewAppBody'}>{children}</body>
 		</html>
 	);
